bf43cc7307e145ea362e34c9f764374e79d308d9
Matthew Yau <myau0005@student.monash.edu>
2025-01-17 10:28:08 +1100

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <meta property="og:description" content={metadata.description} />
         <meta
           property="og:image"
-          content="https://monashjapaneseclub.com/images/embed.png"
+          content="https://monashjapaneseclub.com/images/logo.png"
         />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
