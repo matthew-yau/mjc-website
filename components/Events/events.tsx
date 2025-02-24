6e30cc4cd61178a582ed0fbd774bad594197760f
Matthew Yau <myau0005@student.monash.edu>
2025-02-24 22:20:07 +1100

    event

--- components/Events/events.tsx
+++ components/Events/events.tsx
@@ -13,7 +13,7 @@ const events = [
       en: "Join us for an exciting start to the year during Orientation Week! Discover Japanese culture, meet new friends, and enjoy fun activities and delicious snacks.",
       jp: "オリエンテーション週間で、会員としてエキサイティングな1年の始まりを迎え、日本文化を発見し、新しい友達と出会い、楽しいアクティビティや美味しい軽食を楽しめます。",
     },
-    imageUrl: "/images/meirl.png",
+    imageUrl: "/images/oweek.png",
   },
   {
     title: {
@@ -25,67 +25,67 @@ const events = [
       en: "Indulge in a delightful evening filled with freshly made Yakisoba and experience the flavors of Japan with fellow club members.",
       jp: "会員として、出来立ての焼きそばを楽しみながら、クラブの仲間たちと一緒に日本の味を体験できます。",
     },
-    imageUrl: "/images/place-holder.png",
+    imageUrl: "/images/yakisoba.jpeg",
+  },
+  {
+    title: {
+      en: "Dodgeball Event",
+      jp: "ドッジボールイベント",
+    },
+    date: "Week 4",
+    description: {
+      en: "Get ready for some fast-paced action with a friendly game of dodgeball! Team up and have fun!",
+      jp: "会員は、ドッジボールのゲームでチームを組み、楽しい時間を過ごせます！",
+    },
+    imageUrl: "/images/dodgeball.jpeg",
+  },
+  {
+    title: {
+      en: "Crafts Event",
+      jp: "クラフトイベント",
+    },
+    date: "Week 6",
+    description: {
+      en: "Unleash your creativity and try your hand at Japanese crafts! A fun and relaxing experience awaits.",
+      jp: "会員は、日本のクラフトに挑戦し、楽しくリラックスした時間を過ごせます。",
+    },
+    imageUrl: "/images/craft.png",
+  },
+  {
+    title: {
+      en: "Wagashi Making",
+      jp: "和菓子作り",
+    },
+    date: "Week 8",
+    description: {
+      en: "Learn the art of making traditional Japanese sweets and enjoy the delicious treats you create!",
+      jp: "会員は、伝統的な日本の和菓子作りを学び、自分で作った美味しいお菓子を楽しめます。",
+    },
+    imageUrl: "/images/wagashi.jpg",
   },
   {
     title: {
       en: "Kendo Event",
       jp: "剣道イベント",
     },
-    date: "Week 4",
+    date: "Week 10",
     description: {
       en: "Learn the basics of Kendo, the traditional Japanese martial art. Watch live demonstrations and even try it out yourself!",
       jp: "会員は、日本の伝統的な武道である剣道の基本を学び、実演を見学したり、自分でも体験したりできます。",
     },
-    imageUrl: "/images/place-holder.png",
+    imageUrl: "/images/kendo.jpeg",
   },
   {
     title: {
-      en: "Ice Skating Event",
-      jp: "アイススケートイベント",
-    },
-    date: "Week 6",
-    description: {
-      en: "Enjoy a fun-filled evening of ice skating with your friends. Take a break from studies and glide into an unforgettable experience.",
-      jp: "会員は、友達と楽しいアイススケートの夕べを過ごし、勉強の合間にリフレッシュしながら忘れられない体験を楽しめます。",
-    },
-    imageUrl: "/images/place-holder.png",
-  },
-  {
-    title: {
-      en: "Careers Night",
-      jp: "キャリアナイト",
-    },
-    date: "Week 8",
-    description: {
-      en: "Explore career opportunities and hear from professionals who have bridged Japanese and international industries. Perfect for networking and career advice.",
-      jp: "会員は、日本と国際的な業界をつなぐプロフェッショナルから話を聞き、キャリアの可能性を探り、ネットワーキングやキャリアアドバイスを受けられます。",
-    },
-    imageUrl: "/images/place-holder.png",
-  },
-  {
-    title: {
-      en: "Tea Ceremony Event",
-      jp: "茶道イベント",
-    },
-    date: "Week 10",
-    description: {
-      en: "Immerse yourself in the elegance of a traditional Japanese tea ceremony. Learn about its rich history and cultural significance.",
-      jp: "会員は、伝統的な日本の茶道の優雅さに浸り、その豊かな歴史と文化的意義を学べます。",
-    },
-    imageUrl: "/images/place-holder.png",
-  },
-  {
-    title: {
-      en: "Study Session",
-      jp: "勉強会",
+      en: "Games Night",
+      jp: "ゲームナイト",
     },
     date: "Week 12",
     description: {
-      en: "Join us for a productive study session to prepare for finals. Share tips, stay motivated, and enjoy light refreshments with club members.",
-      jp: "会員は、期末試験に向けた効率的な勉強会に参加し、コツを共有し、モチベーションを維持しながら、クラブメンバーと軽食を楽しめます。",
+      en: "Join us for a night of fun and games! Play a variety of Japanese and international board games.",
+      jp: "会員は、日本や国際的なボードゲームを楽しめるゲームナイトに参加できます。",
     },
-    imageUrl: "/images/place-holder.png",
+    imageUrl: "/images/games.jpg",
   },
 ];
 
@@ -101,8 +101,13 @@ const Events: React.FC<{ language: "en" | "jp" }> = ({ language }) => {
         {events.map((event, index) => (
           <div
             key={index}
-            className="bg-white shadow-lg rounded-lg flex flex-col overflow-hidden"
+            className="bg-white shadow-lg rounded-lg flex flex-row overflow-hidden"
           >
+            <img
+              src={event.imageUrl}
+              alt={event.title.en}
+              className="w-1/3 object-cover"
+            />
             <div className="p-4 sm:p-6 flex flex-col justify-between flex-grow">
               <div>
                 <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
