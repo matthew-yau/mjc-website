54804554ac02aca50b6349c4ac20662ebb5d2ae4
Matthew Yau <myau0005@student.monash.edu>
2025-03-29 02:29:14 +1100

    hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
