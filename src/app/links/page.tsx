037ded8d7d717cb472440ac5858e7601187167c1
Matthew Yau <myau0005@student.monash.edu>
2025-08-08 19:46:25 +0900

    stuff

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -34,22 +34,16 @@ const LinksPage: React.FC = () => {
 
   const links = [
     {
-      title_en: "Games Night Sign-up",
-      title_jp: "ゲームナイト登録",
-      url: "https://docs.google.com/forms/d/e/1FAIpQLSeQZGYsH5t-G0MxB8DnpjpWaKyUF1188e80cdVcn0LxFDL9IQ/viewform",
-      icon: <FaGamepad className="text-2xl" />,
-    },
-    {
-      title_en: "MJC x MUJC Ball Tickets",
-      title_jp: "千灯祭登録",
-      url: "https://www.trybooking.com/events/landing/1401090",
+      title_en: "Matcha Workshop Session 1 (4:30-5:30pm)",
+      title_jp: "抹茶ワークショップ セッション1 (16:30-17:30)",
+      url: "https://www.eventbrite.com.au/e/matcha-workshop-session-1-430pm-530pm-tickets-1536489925729?aff=oddtdtcreator",
       icon: <BsTicketDetailed className="text-2xl" />,
     },
     {
-      title_en: "MJC x MUJC Ball Details",
-      title_jp: "千灯祭詳細",
-      url: "/ball",
-      icon: <GiLantern className="text-2xl" />,
+      title_en: "Matcha Workshop Session 2 (6:00-7:00pm)",
+      title_jp: "抹茶ワークショップ セッション2 (18:00-19:00)",
+      url: "https://www.eventbrite.com.au/e/matcha-workshop-session-2-600pm-700pm-tickets-1536523315599?aff=oddtdtcreator",
+      icon: <BsTicketDetailed className="text-2xl" />,
     },
     {
       title_en: "Club Sign-up",
